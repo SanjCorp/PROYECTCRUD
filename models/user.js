adbb67f7750398278fb809383511e8a88a28b78b
ricardo <richisanj@gmail.com>
2025-10-13 12:06:12 -0400

    Remove bcrypt import from user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,23 +17,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
 });
-
-// Antes de guardar, hashea la contraseña
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-// Método para comparar contraseña
-userSchema.methods.comparePassword = async function (password) {
-  return bcrypt.compare(password, this.password);
-};
 
 export const User = mongoose.model("User", userSchema);
